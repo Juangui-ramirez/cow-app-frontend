bff11567d3c81872478818ee96fc4178c48606d8
Juangui <juan-ramirez04@hotmail.com>
2024-04-01 18:36:29 -0500

    adding temparally home and function get

--- cow-app/src/pages/Home.jsx
+++ cow-app/src/pages/Home.jsx
@@ -1,9 +1,38 @@
+import { useState, useEffect } from "react";
+
 export function Home() {
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    const pets = fetch("http://localhost:3000/pets");
+    pets.then(
+      (res) =>
+        res.json().then((data) => {
+          console.info("response", data);
+          setPets(data);
+        }),
+      (err) => {
+        console.info("request error", err);
+      }
+    );
+  }, []);
+
+
     return (
       <div className="p-4 bg-blue-200">
         <p className="text-xl text-gray-700">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. A sint eaque iste minima alias in totam blanditiis, necessitatibus ullam quam optio fuga inventore illo tempore consequuntur delectus repellat vel nam.
         </p>
+
+        <div>
+            <div>
+              Pets
+            </div>
+            {
+              pets.map(pet => (<div key={pet.name}>{pet.name}</div>))
+            }
+            
+          </div>
       </div>
     );
   }
