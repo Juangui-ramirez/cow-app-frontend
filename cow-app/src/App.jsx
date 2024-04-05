60ff2b70510746c4c66ff2a124595bec8f047594
Juangui <juan-ramirez04@hotmail.com>
2024-04-04 20:48:22 -0500

    Added route por any group

--- cow-app/src/App.jsx
+++ cow-app/src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {Header} from "./components/Header";
-import {Footer} from "./components/Footer";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 import { Home } from "./pages/Home";
 import { Groups } from "./pages/Groups";
 import { Bills } from "./pages/Bills";
 import { Friends } from "./pages/Friends";
-
+import { GroupDetails } from "./pages/GroupDetails";
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
           <Route path="/friends" element={<Friends />} />
           <Route path="/bills" element={<Bills />} />
           <Route path="/groups" element={<Groups />} />
+          <Route path="/groups/:groupName" element={<GroupDetails />} />
         </Routes>
         <Footer />
       </BrowserRouter>
