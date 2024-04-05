aa40d06f4d16d0694cccaf260adf9de40ad36141
Juangui <juan-ramirez04@hotmail.com>
2024-04-04 21:55:21 -0500

    Added custom colors

--- cow-app/tailwind.config.js
+++ cow-app/tailwind.config.js
@@ -7,7 +7,11 @@ export default {
         sans: ['Fredoka', 'sans-serif'],
       },
       colors:{
-        'brownppal': '#36190D'
+        'brownppal': '#36190D',
+        'amarello' : '#FFA72F',
+        'brownsec'  : '582B1C',
+        'rederror' :  '#FF2530',
+        'greensucess' : '#66B04C'
       }
     },
   },
