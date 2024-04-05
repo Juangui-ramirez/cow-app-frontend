8c656ca5a8a48df66291dc81cb519f36bd6a06a1
Juangui <juan-ramirez04@hotmail.com>
2024-04-04 20:47:37 -0500

    Remove div map

--- cow-app/src/components/GroupCard.jsx
+++ cow-app/src/components/GroupCard.jsx
@@ -1,37 +1,31 @@
 import PropTypes from "prop-types";
 import Logo from "../assets/Logo.svg";
+import { Link } from "react-router-dom";
 
 export const GroupCard = ({ data }) => {
   return (
     <>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
-        {data.map((group) => (
-          <div
-            key={group.name}
-            className="flex gap-5 border-b-2 shadow-xl pl-3 mb-4"
-          >
-            <img
-              src={Logo}
-              alt=""
-              className="p-2 rounded-md w-[70px] mb-4"
-              style={{ backgroundColor: group.color }}
-            />
-            <div className="flex flex-col justify-between mb-4">
-              <h2 className="font-bold">{group.name}</h2>
-              <p className="font-bold">
-                You owe: <span className="text-red-600">$12.000</span>
-              </p>
-              <div className="flex gap-2">
-                <button className="bg-brownppal text-white rounded-md h-auto max-w-[6rem] w-28">
-                  Edit
-                </button>
-                <button className="bg-brownppal text-white rounded-md h-auto max-w-[6rem] w-28">
-                  Delete
-                </button>
-              </div>
-            </div>
-          </div>
-        ))}
+      <img
+        src={Logo}
+        alt=""
+        className="p-2 rounded-md w-[70px] mb-4"
+        style={{ backgroundColor: data.color }}
+      />
+      <div className="flex flex-col justify-between mb-4">
+        <h2 className="font-bold">{data.name}</h2>
+        <p className="font-bold">
+          You owe: <span className="text-red-600">$12.000</span>
+        </p>
+        <div className="flex gap-2">
+          <Link to={`/groups/${data.name}`}>
+            <button className="bg-brownppal text-white rounded-md h-auto max-w-[6rem] w-28">
+              Edit
+            </button>
+          </Link>
+          <button className="bg-brownppal text-white rounded-md h-auto max-w-[6rem] w-28">
+            Delete
+          </button>
+        </div>
       </div>
     </>
   );
