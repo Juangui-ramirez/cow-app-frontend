027c0bd3755754836a0c921b62979e56c970106f
Juangui <juan-ramirez04@hotmail.com>
2024-07-04 19:10:44 -0500

    add footer on unauth

--- cow-app/src/App.jsx
+++ cow-app/src/App.jsx
@@ -35,7 +35,7 @@ function App() {
           path="/unauthorized"
           element={
             <>
-              <Unauthorized />
+              <Unauthorized /> <Footer />
             </>
           }
         />
